f13263f2da1feb0f73324e8bc484f3c3b21f060e
gussiciliano <gussiciliano@gmail.com>
2018-10-29 18:07:05 -0300

    wip

--- src/Models/Views/BaseEntityViewModel.cs
+++ src/Models/Views/BaseEntityViewModel.cs
@@ -5,15 +5,6 @@ namespace AssignmentsNetcore.Models.Views
 {
     public class BaseEntityViewModel
     {
-        public BaseEntityViewModel() { }
-
-        public BaseEntityViewModel(BaseEntity m)
-        {
-            this.Id = m.Id;
-            this.CreatedAt = m.CreatedAt;
-            this.UpdatedAt = m.UpdatedAt;
-        }
-
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
