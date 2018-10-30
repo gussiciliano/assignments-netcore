c696e0067e5f237ee833bd4c17c6d412297c397e
Gustavo Siciliano <gussiciliano@gmail.com>
2018-10-30 18:16:14 -0300

    wip

--- src/Controllers/Backoffice/AssignmentController.cs
+++ src/Controllers/Backoffice/AssignmentController.cs
@@ -1,13 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using AssignmentsNetcore.Models.Database;
-using AssignmentsNetcore.Repositories.Database;
 using AssignmentsNetcore.Controllers.Backoffice;
+using AssignmentsNetcore.Models.Database;
 using AssignmentsNetcore.Models.Views;
 using AssignmentsNetcore.Repositories.Interfaces;
 
@@ -36,5 +29,4 @@ namespace AssignmentsNetcore.Controllers
             throw new NotImplementedException();
         }
     }
-
 }
